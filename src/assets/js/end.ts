c6497a000bce213784feb17635c629c7a7d4a7ab
terracotta <begaiym.akylova@gmail.com>
2022-10-12 14:25:24 +0600

    Add styles for end page

--- src/assets/js/end.ts
+++ src/assets/js/end.ts
@@ -8,7 +8,7 @@ import "../scss/main.scss";
 
 const currentUser = JSON.parse(localStorage.getItem("currentUser")!);
 
-const scoreText = /[1-4]+$/.test(currentUser.score) ? "очка" : "очков";
+const scoreText = /[1]+$/.test(currentUser.score) ? "point" : "points";
 score.textContent = `${currentUser.score} ${scoreText}`;
 usernameEndPage.textContent = currentUser.username;
 rightAnswers.textContent = currentUser.right;
